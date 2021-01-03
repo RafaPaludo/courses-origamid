d28b0b8a7540f74cd0f96588ca8abd429eed3c49
RafaPaludo <rafaelricardopp@gmail.com>
2021-01-03 02:49:41 -0300

    chore: change html files to .php, add wordpress functions and config templates

---
+++ Wordpress_como_CMS/theme-rest/footer.php
@@ -0,0 +1,14 @@
+        <footer>
+			<nav class="nav-footer">
+				<ul>
+					<li class="current_page_item"><a href="/hendel/">Menu</a></li>
+					<li><a href="/hendel/sobre/">Sobre</a></li>
+					<li><a href="/hendel/contato/">Contato</a></li>
+				</ul>
+			</nav>
+
+			<p>Rest © 2015. Alguns direitos reservados.</p>
+		</footer>
+        <?php wp_footer(); ?>
+	</body>
+</html>
