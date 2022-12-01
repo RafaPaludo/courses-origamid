eda87c74668a7d4c07f980ab3eb313d638a6f0f3
Rafa Paludo <rafael.ricardopp@gmail.com>
2022-12-01 10:55:55 -0300

    init: create fake api, add some assets and make product cards

---
+++ Vuejs/Vue_04/Techno_project/app.js
@@ -0,0 +1,29 @@
+const vm = new Vue({
+    el: '#app',
+    data: {
+        store: 'Techno',
+        message: 'Hello World',
+        products: [],
+        selectedProduct: {},
+        showModal: false
+    },
+    methods: {
+        fetchProducts () {
+            fetch('./api/produtos.json')
+                .then( r => r.json() )
+                .then( r => this.products = r )
+        },
+        fetchFullProduct (id) {
+            fetch(`./api/produtos/${id}/dados.json`)
+                .then( r => r.json() )
+                .then( r => this.selectedProduct = r )
+        },
+        openModal(id) {
+            this.fetchFullProduct(id)
+            this.showModal = true
+        }
+    },
+    created () {
+        this.fetchProducts()
+    }
+})
