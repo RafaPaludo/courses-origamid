28f9f2eff6d1faf1685c7af8f56142ab2f38ef84
Rafa Paludo <rafael.ricardopp@gmail.com>
2022-12-06 00:21:49 -0300

    feat: create cart modal, modal product added

--- Vuejs/Vue_04/Techno_project/app.js
+++ Vuejs/Vue_04/Techno_project/app.js
@@ -5,7 +5,11 @@ const vm = new Vue({
         message: 'Hello World',
         products: [],
         selectedProduct: {},
-        showModal: false
+        showModal: false,
+        productAdded: false,
+        stockControl: [],
+        cart: [],
+        cartModalOpen: false
     },
     methods: {
         fetchProducts () {
@@ -24,7 +28,35 @@ const vm = new Vue({
         },
         closeModal() {
             this.showModal = false
-        }
+        }, 
+        showModalCartAdded () {
+            setTimeout( () => this.productAdded = true, 500)
+            setTimeout( () => this.productAdded = false, 3000)
+        },
+        closeCartModal() {
+            this.cartModalOpen = false
+        },
+        openCartModal() {
+            this.cartModalOpen = true
+        },
+        finish () {
+            window.alert('Finalizado')
+        },
+        addProductToCart () {
+            const { id, nome, preco, estoque } = this.selectedProduct
+            const hasStock = this.checkStock(id, estoque)
+            this.cart.push({ id, nome, preco, estoque })
+            this.stockControl.push({ id, nome, preco, estoque })
+            console.log(this.cart)
+        },
+        checkStock (id, estoque) {
+            const stock = this.stockControl.filter( prod => prod.id === this.selectedProduct.id)
+            if(stock){
+                this.stockControl--
+                return true
+            }
+            console.log(stock)
+        },
     },
     created () {
         this.fetchProducts()
