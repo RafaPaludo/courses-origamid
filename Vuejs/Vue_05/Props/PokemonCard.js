dae466db616137792bfa6dd64dd5a04688b08fc5
Rafa Paludo <rafael.ricardopp@gmail.com>
2022-12-14 11:18:17 -0300

    feat: fetch api using props

---
+++ Vuejs/Vue_05/Props/PokemonCard.js
@@ -0,0 +1,69 @@
+export default {
+    name: 'PokemonCard',
+    template: `
+        <div
+            class="card_wrapper"
+            v-if="pokemon"
+        >
+            <div class="card_name">
+                {{ pokemon.name }}
+            </div>
+
+            <div class="card_img">
+                <img :src="sanitizedImg('front_default')" :alt="pokemon.name" class="card_img_front">
+                <img :src="sanitizedImg('back_default')" :alt="pokemon.name" class="card_img_back">
+            </div>
+
+            <div class="card_types">
+                <div
+                    v-for="{ type} in pokemon.types"
+                    class="card_type"
+                >
+                    {{ type.name }}
+                </div>
+            </div>
+        </div>
+    `,
+    data () {
+        return {
+            pokemon: ''
+        }
+    },
+    props: {
+        id: {
+            type: Number,
+            require: false,
+            validator: function (value) {
+                return value < 905
+            }
+        }
+    },
+    methods: {
+        generateID () {
+            return Math.floor(Math.random() * 905)
+        },
+        fetchPokemon () {
+            const pokemonID = this.id || this.generateID()
+
+            fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`)
+                .then( r => r.json() )
+                .then( r => this.pokemon = r  )
+                .catch( err => console.error('Api não retornou o pokemon, erro: ', err ) )
+            
+        },
+        sanitizedImg (spriteType) {
+            return this.pokemon.sprites[spriteType]
+        }
+    },
+    created () {
+        this.fetchPokemon()
+    },
+    watch: {
+        id () {
+            this.fetchPokemon()
+        }
+    }
+}
+
+// Retorna dados de um pokemon:: https://pokeapi.co/api/v2/pokemon/{ ID or NAME }
+// API:: https://pokeapi.co/docs/v2#pokemon
